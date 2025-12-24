b237ed6ac41c208c983cad7bc3a086366ba0fd16
Feras Taleb <6038666+ferastaleb@users.noreply.github.com>
2025-12-24 23:31:18 +0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// for filter events in events.html 
 document.getElementById("categoryFilter")?.addEventListener("change", function () {
   let value = this.value;
   let events = document.querySelectorAll(".event-item");
@@ -9,4 +10,20 @@ document.getElementById("categoryFilter")?.addEventListener("change", function (
       event.style.display = "none";
     }
   });
+});
+
+// for contact form validation in contact.html
+document.getElementById("contactForm")?.addEventListener("submit", function(e){
+  e.preventDefault();
+
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+
+  let alertBox = document.getElementById("alertBox");
+
+  if(name === "" || email === ""){
+    alertBox.innerHTML = "<div class='alert alert-danger'>يرجى تعبئة الحقول</div>";
+  } else {
+    alertBox.innerHTML = "<div class='alert alert-success'>تم الإرسال بنجاح</div>";
+  }
 });
